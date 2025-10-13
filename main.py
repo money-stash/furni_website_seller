93806f1b7eb44a3a0dd6a467ff9019ef4bd23c79
maksEveline <maksimeveline@gmail.com>
2025-10-13 22:16:30 +0300

    reworked admin panel view

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
+app.secret_key = "Lql8aLsBzUVWvY6Ood1egDyanmTwN2GV"  # обязательно для сессий
+
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "1234"
 
 
 @app.route("/")
@@ -26,6 +30,37 @@ def services():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/admin-login", methods=["GET", "POST"])
+def admin_login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+            session["admin_logged_in"] = True
+            return redirect(url_for("admin_panel"))
+        else:
+            flash("Неправильный логин или пароль", "error")
+            return redirect(url_for("admin_login"))
+
+    return render_template("admin-login.html")
+
+
+@app.route("/admin-panel")
+def admin_panel():
+    if not session.get("admin_logged_in"):
+        return redirect(url_for("admin_login"))
+
+    return render_template("admin-panel.html")
+
+
+@app.route("/admin-logout")
+def admin_logout():
+    session.pop("admin_logged_in", None)
+
+    return redirect(url_for("admin_login"))
 
 
 if __name__ == "__main__":
